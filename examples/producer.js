eff9156c5d69168e09dce569f93372d33cf9d466
Rob Skillington <rob.skillington@gmail.com>
2013-03-05 21:18:11 -0800

    Correctly respond to scribe Log method requests

--- examples/producer.js
+++ examples/producer.js
@@ -1,4 +1,4 @@
 var scribely = require('../lib/scribely/scribely.js');
 
 var producer = new scribely.Producer('localhost', 1463);
-producer.send('default', 'hello world');
+producer.send('default', 'hello world from scribely');
